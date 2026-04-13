159670da171719b570cdb4eb72b15a9588edf585
PrinzOji <ojiemdennis67@gmail.com>
2026-04-13 16:36:14 +0300

    initial commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .forms import StudentForm
 
 # Create your views here.
 def contacts(request):
     return render(request, 'contacts.html')
+
+def contact2(request):
+    context = {}
+    form = StudentForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            context['success_message'] = "Form submitted successfully!"
+        else:
+            context['error_message'] = "There was an error submitting the form. Please check the details and try again."
+
+    context['form'] = form
+
+    return render(request, 'contact2.html', context)
